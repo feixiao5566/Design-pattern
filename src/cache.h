8d62f59c9981986a830ca4f7bcb4a539a0888973
feixiao5566 <feixiao5566@qq.com>
2015-11-25 17:56:27 +0800

    策略模式 模板 模板理解

--- src/cache.h
+++ src/cache.h
@@ -16,6 +16,12 @@ class Cache
             m_ra.Replace();
         }
     private:
-        RA m_ra;
+        RA m_ra;    //这意思是给RA的对象么?     是的
+                    //但这还只是个模板的声明,这个时候的RA与stratery没啥关系
+                    //在main函数里,有
+                    //Cache<Random_ReplaceAlgorithm> cache; //模板实参
+                    //这句才..
+                    //相当于把RA的构造函数写在Cache
+                    //main中初始化Cache是顺便就把RA给初始化了o_o
 };
 #endif
